3c5437df0fc1fd12e549810d1207ed59a89be75b
PadmaSunitha <133723159+PadmaSunitha@users.noreply.github.com>
2023-05-24 10:27:13 -0400

    Update streamlit_app.py
    added stop statement to trouble shoot

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_ch
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+#dont run anything past here while we trobleshoot
+streamlit.stop()
 
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
